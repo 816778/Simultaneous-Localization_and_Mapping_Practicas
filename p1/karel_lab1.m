a3cc3c9c0c4f682d46af85b7e449156c2fd5a890
816778 <816778@unizar.es>
2025-01-31 11:52:17 +0100

    fixing update_map

--- p1/karel_lab1.m
+++ p1/karel_lab1.m
@@ -11,8 +11,6 @@ close all; % figures
 randn('state', 1); % always use same random number sequence
 rand('state', 1); % always use same random number sequence
 format long
-% En mi caso concreto tengo que poner set(gcf, 'Renderer', 'painters');
-% después de cada figure porque sino no me aparecen las figuras en pantalla
 %-------------------------------------------------------------------------
 % configuration parameters
 
@@ -118,60 +116,60 @@ display_map_results (map);
 
 function[map] = Kalman_filter_slam (map, steps)
 
-global world;
-
-% initial ground truth of hat_x 
-map.true_x = zeros(steps);
-map.true_x = [0];
-
-% initial number of features
-map.n = 0;
-
-% initial map state and covariance
-map.R0 = world.true_robot_location;
-
-map.hat_x = zeros(steps);
-map.hat_x = [0];
-
-map.hat_P = zeros(steps,steps);
-map.hat_P = [0];
-
-% feature ids, for robot = 0
-map.true_ids = [0];
-
-% useful statistics
-map.stats.true_x = [0];
-map.stats.error_x = [];
-map.stats.sigma_x = [];
-map.stats.cost_t = [];
-
-for k = 0:steps
+    global world;
     
-    tstart = tic;
+    % initial ground truth of hat_x 
+    map.true_x = zeros(steps);
+    map.true_x = [0];
     
-    if k > 0
-        [map] = compute_motion (map);
+    % initial number of features
+    map.n = 0;
+    
+    % initial map state and covariance
+    map.R0 = world.true_robot_location;
+    
+    map.hat_x = zeros(steps);
+    map.hat_x = [0];
+    
+    map.hat_P = zeros(steps,steps);
+    map.hat_P = [0];
+    
+    % feature ids, for robot = 0
+    map.true_ids = [0];
+    
+    % useful statistics
+    map.stats.true_x = [0];
+    map.stats.error_x = [];
+    map.stats.sigma_x = [];
+    map.stats.cost_t = [];
+    
+    for k = 0:steps
+        
+        tstart = tic;
+        
+        if k > 0
+            [map] = compute_motion (map);
+        end
+        
+        % get measurements
+        [measurements] = get_measurements_and_data_association(map);
+        
+        % seen features already in the map? KF update!
+        if not(isempty(measurements.z_f))
+            [map] = update_map (map, measurements);
+        end
+        
+        % some new features?
+        if not(isempty(measurements.z_n))
+            [map] = add_new_features (map, measurements);
+        end
+        
+        % record statistics
+        map.stats.error_x = [map.stats.error_x; (map.stats.true_x(end) - map.hat_x(1))];
+        map.stats.sigma_x = [map.stats.sigma_x; sqrt(map.hat_P(1,1))];
+        map.stats.cost_t = [map.stats.cost_t; toc(tstart)];
+        
     end
-    
-    % get measurements
-    [measurements] = get_measurements_and_data_association(map);
-    
-    % seen features already in the map? KF update!
-    if not(isempty(measurements.z_f))
-        [map] = update_map (map, measurements);
-    end
-    
-    % some new features?
-    if not(isempty(measurements.z_n))
-        [map] = add_new_features (map, measurements);
-    end
-    
-    % record statistics
-    map.stats.error_x = [map.stats.error_x; (map.stats.true_x(end) - map.hat_x(1))];
-    map.stats.sigma_x = [map.stats.sigma_x; sqrt(map.hat_P(1,1))];
-    map.stats.cost_t = [map.stats.cost_t; toc(tstart)];
-    
-end
 end
 
 %-------------------------------------------------------------------------
@@ -180,30 +178,30 @@ end
 
 function [map] = compute_motion (map)
 
-global config;
-global robot;
-global world;
-
-% move robot and update ground truth robot location
-world.true_robot_location = world.true_robot_location + robot.true_uk;
-
-% odometry error
-sigma_xk = robot.factor_x * robot.true_uk;
-error_k = randn(1)*sigma_xk;
-
-% estimated motion
-map.hat_x(1) = map.hat_x(1) + robot.true_uk + error_k;
-map.hat_P(1,1) = map.hat_P(1,1) + sigma_xk^2;
-
-% update ground truth for the current map
-map.true_x(1) = map.true_x(1) + robot.true_uk;
-map.stats.true_x = [map.stats.true_x; map.stats.true_x(end) + robot.true_uk];
-
-if config.step_by_step
-    fprintf('Move to %d...\n',map.true_x(1));
-    plot_correlation(map.hat_P);
-    pause
-end
+    global config;
+    global robot;
+    global world;
+    
+    % move robot and update ground truth robot location
+    world.true_robot_location = world.true_robot_location + robot.true_uk;
+    
+    % odometry error
+    sigma_xk = robot.factor_x * robot.true_uk;
+    error_k = randn(1)*sigma_xk;
+    
+    % estimated motion
+    map.hat_x(1) = map.hat_x(1) + robot.true_uk + error_k;
+    map.hat_P(1,1) = map.hat_P(1,1) + sigma_xk^2;
+    
+    % update ground truth for the current map
+    map.true_x(1) = map.true_x(1) + robot.true_uk;
+    map.stats.true_x = [map.stats.true_x; map.stats.true_x(end) + robot.true_uk];
+    
+    if config.step_by_step
+        fprintf('Move to %d...\n',map.true_x(1));
+        plot_correlation(map.hat_P);
+        pause
+    end
 end
 
 %-------------------------------------------------------------------------
@@ -212,35 +210,35 @@ end
 
 function [measurements] = get_measurements_and_data_association(map)
 
-global world;
-global sensor;
-
-distances = world.true_point_locations - world.true_robot_location;
-visible_ids = find((distances >= sensor.range_min) & (distances <= sensor.range_max));
-visible_d = distances(visible_ids);
-n_visible = length(visible_ids);
-
-% sensor error
-sigma_z = sensor.factor_z * visible_d;
-error_z = randn(n_visible, 1) .* sigma_z;
-
-measurements.z_k = visible_d + error_z;
-measurements.R_k = diag(sigma_z.^2);
-
-% data association
-measurements.ids_f = intersect(map.true_ids, visible_ids);
-measurements.ids_n = setdiff(visible_ids, map.true_ids);
-measurements.z_pos_f = find(ismember(visible_ids, measurements.ids_f));
-measurements.z_pos_n = find(ismember(visible_ids, measurements.ids_n));
-measurements.x_pos_f = find(ismember(map.true_ids, visible_ids));
-
-%features already in the map
-measurements.z_f = measurements.z_k(measurements.z_pos_f);
-measurements.R_f = measurements.R_k(measurements.z_pos_f,measurements.z_pos_f);
-
-%new features
-measurements.z_n = measurements.z_k(measurements.z_pos_n);
-measurements.R_n = measurements.R_k(measurements.z_pos_n,measurements.z_pos_n);
+    global world;
+    global sensor;
+    
+    distances = world.true_point_locations - world.true_robot_location;
+    visible_ids = find((distances >= sensor.range_min) & (distances <= sensor.range_max));
+    visible_d = distances(visible_ids);
+    n_visible = length(visible_ids);
+    
+    % sensor error
+    sigma_z = sensor.factor_z * visible_d;
+    error_z = randn(n_visible, 1) .* sigma_z;
+    
+    measurements.z_k = visible_d + error_z;
+    measurements.R_k = diag(sigma_z.^2);
+    
+    % data association
+    measurements.ids_f = intersect(map.true_ids, visible_ids);
+    measurements.ids_n = setdiff(visible_ids, map.true_ids);
+    measurements.z_pos_f = find(ismember(visible_ids, measurements.ids_f));
+    measurements.z_pos_n = find(ismember(visible_ids, measurements.ids_n));
+    measurements.x_pos_f = find(ismember(map.true_ids, visible_ids));
+    
+    %features already in the map
+    measurements.z_f = measurements.z_k(measurements.z_pos_f);
+    measurements.R_f = measurements.R_k(measurements.z_pos_f,measurements.z_pos_f);
+    
+    %new features
+    measurements.z_n = measurements.z_k(measurements.z_pos_n);
+    measurements.R_n = measurements.R_k(measurements.z_pos_n,measurements.z_pos_n);
 
 end
 
@@ -250,16 +248,42 @@ end
 
 function[map] = update_map (map, measurements)
 
-global config;
+    global config;
+    
+    % DO SOMETHING HERE!
+    % You need to compute H_k, y_k, S_k, K_k and update map.hat_x and map.hat_P
+    n_f = length(measurements.ids_f);
+    if n_f == 0
+        return; % No hay características
+    end
 
-% DO SOMETHING HERE!
-% You need to compute H_k, y_k, S_k, K_k and update map.hat_x and map.hat_P
+    z_f = measurements.z_f;  % Distancia medida a características conocidas
+    R_f = measurements.R_f;  % Covarianza de la medición
 
-if config.step_by_step
-    fprintf('Updape map for last %d features...\n',length(measurements.ids_f));
-    plot_correlation(map.hat_P);
-    pause
-end
+    x_r = map.hat_x(1);  % Posición estimada del robot
+    x_f = map.hat_x(measurements.x_pos_f);  % Posiciones estimadas de características
+
+    h_x = x_f - x_r;
+    y_k = z_f - h_x;
+    
+    % Construcción de la matriz de observación H_k
+    H_k = sparse(n_f, length(map.hat_x));  %  matriz sparse
+    for i = 1:n_f
+        H_k(i, 1) = -1; 
+        H_k(i, measurements.x_pos_f(i)) = 1;
+    end
+
+    S_k = H_k * map.hat_P * H_k' + R_f;
+    % Ganancia de Kalman K_k
+    K_k = map.hat_P * H_k' / S_k;
+    map.hat_x = map.hat_x + K_k * y_k;
+    map.hat_P = (speye(size(map.hat_P)) - K_k * H_k) * map.hat_P;
+
+    if config.step_by_step
+        fprintf('Updape map for last %d features...\n',length(measurements.ids_f));
+        plot_correlation(map.hat_P);
+        pause
+    end
 end
 
 %-------------------------------------------------------------------------
@@ -267,18 +291,36 @@ end
 %-------------------------------------------------------------------------
 
 function [map] = add_new_features (map, measurements)
-
-global config;
-global world;
-
-% DO SOMETHING HERE!
-% update map.hat_x, map.hat_P, map.true_ids, map.true_x, map.n
-
-if config.step_by_step
-    fprintf('Add %d new features...\n',length(measurements.ids_n));
-    plot_correlation(map.hat_P);
-    pause
-end
+    global config;
+    global world;
+    
+    % update map.hat_x, map.hat_P, map.true_ids, map.true_x, map.n
+    % Number of new features
+    n_new = length(measurements.ids_n);
+    if n_new == 0
+        return;
+    end
+    % Extract new measurements and their covariance
+    z_n = measurements.z_n;  % Measured distances
+    R_n = measurements.R_n;  % Measurement covariances
+    n_old = map.n; % Current number of features in the map
+    
+    J1 = sparse(eye(n_old+1));
+    extra_rows = sparse([ones(n_new,1), zeros(n_new, n_old)]);
+    J1 = [J1; extra_rows];
+    J2 = sparse([zeros(n_old + 1, n_new); eye(n_new)]);    
+    map.hat_x = J1 * map.hat_x + J2 * z_n;
+    map.hat_P = J1 * map.hat_P * J1' + J2 * R_n * J2';
+   
+    map.true_ids = [map.true_ids; measurements.ids_n];  % Append new feature IDs
+    map.true_x = [map.true_x; map.true_x(1) + z_n];  % True feature positions
+    map.n = n_old + n_new;
+    
+    if config.step_by_step
+        fprintf('Add %d new features...\n',length(measurements.ids_n));
+        plot_correlation(map.hat_P);
+        pause
+    end
 end
 
 %-------------------------------------------------------------------------
@@ -287,59 +329,59 @@ end
 
 function  display_map_results (map)
 
-global config;
-
-config.fig = config.fig + 1;
-figure(config.fig);
-set(gcf, 'Renderer', 'painters');
-axis([0 length(map.hat_x) -2*max(sqrt(diag(map.hat_P))) 2*max(sqrt(diag(map.hat_P)))]);
-grid on;
-hold on;
-plot(map.true_ids, map.hat_x-map.true_x, 'ro','Linewidth', 2);
-plot(map.true_ids, 2*sqrt(diag(map.hat_P)), 'b+','Linewidth', 2);
-plot(map.true_ids, -2*sqrt(diag(map.hat_P)), 'b+','Linewidth', 2);
-xlabel('Feature number (robot = 0)');
-ylabel('meters (m)');
-title('Map estimation error + 2sigma bounds');
-
-config.fig = config.fig + 1;
-figure(config.fig);
-set(gcf, 'Renderer', 'painters');
-plot_correlation(map.hat_P);
-title(sprintf('Correlation matrix of size %d', size(map.hat_P,1)));
-
-config.fig = config.fig + 1;
-figure(config.fig);
-set(gcf, 'Renderer', 'painters');
-grid on;
-hold on;
-plot(map.stats.true_x, map.stats.cost_t,'r-','Linewidth',2);
-xlabel('step');
-ylabel('seconds');
-title('Cost per step');
-
-config.fig = config.fig + 1;
-figure(config.fig);
-set(gcf, 'Renderer', 'painters');
-grid on;
-hold on;
-plot(map.stats.true_x,cumsum(map.stats.cost_t),'r-','Linewidth',2);
-xlabel('step');
-ylabel('seconds');
-title('Cumulative cost');
-
-config.fig = config.fig + 1;
-figure(config.fig);
-set(gcf, 'Renderer', 'painters');
-axis([0 map.stats.true_x(end) -2*max(map.stats.sigma_x) 2*max(map.stats.sigma_x)]);
-grid on;
-hold on;
-plot(map.stats.true_x, map.stats.error_x,'r-','Linewidth',2);
-plot(map.stats.true_x, 2*map.stats.sigma_x,'b-','Linewidth',2);
-plot(map.stats.true_x,-2*map.stats.sigma_x,'b-','Linewidth',2);
-xlabel('meters (m)');
-ylabel('meters (m)');
-title('Robot estimation error + 2sigma bounds');
+    global config;
+    
+    config.fig = config.fig + 1;
+    figure(config.fig);
+    
+    axis([0 length(map.hat_x) -2*max(sqrt(diag(map.hat_P))) 2*max(sqrt(diag(map.hat_P)))]);
+    grid on;
+    hold on;
+    plot(map.true_ids, map.hat_x-map.true_x, 'ro','Linewidth', 2);
+    plot(map.true_ids, 2*sqrt(diag(map.hat_P)), 'b+','Linewidth', 2);
+    plot(map.true_ids, -2*sqrt(diag(map.hat_P)), 'b+','Linewidth', 2);
+    xlabel('Feature number (robot = 0)');
+    ylabel('meters (m)');
+    title('Map estimation error + 2sigma bounds');
+    
+    config.fig = config.fig + 1;
+    figure(config.fig);
+    
+    plot_correlation(map.hat_P);
+    title(sprintf('Correlation matrix of size %d', size(map.hat_P,1)));
+    
+    config.fig = config.fig + 1;
+    figure(config.fig);
+    
+    grid on;
+    hold on;
+    plot(map.stats.true_x, map.stats.cost_t,'r-','Linewidth',2);
+    xlabel('step');
+    ylabel('seconds');
+    title('Cost per step');
+    
+    config.fig = config.fig + 1;
+    figure(config.fig);
+    
+    grid on;
+    hold on;
+    plot(map.stats.true_x,cumsum(map.stats.cost_t),'r-','Linewidth',2);
+    xlabel('step');
+    ylabel('seconds');
+    title('Cumulative cost');
+    
+    config.fig = config.fig + 1;
+    figure(config.fig);
+    
+    axis([0 map.stats.true_x(end) -2*max(map.stats.sigma_x) 2*max(map.stats.sigma_x)]);
+    grid on;
+    hold on;
+    plot(map.stats.true_x, map.stats.error_x,'r-','Linewidth',2);
+    plot(map.stats.true_x, 2*map.stats.sigma_x,'b-','Linewidth',2);
+    plot(map.stats.true_x,-2*map.stats.sigma_x,'b-','Linewidth',2);
+    xlabel('meters (m)');
+    ylabel('meters (m)');
+    title('Robot estimation error + 2sigma bounds');
 
 end
 
@@ -348,19 +390,18 @@ end
 %-------------------------------------------------------------------------
 
 function plot_correlation(P)
-
-ncol=256 ;
-
-% hot cold colormap
-cmap=hsv2rgb([linspace(2/3, 0, ncol)' 0.9*ones(ncol,1) ones(ncol,1)]) ;
-cmap(1,:)=[0 0 0] ;
-colormap(cmap) ;
-
-corr = correlation(P);
-imagesc(abs(corr), [0 1]) ;  % no sign
-
-axis image;
-colorbar ;
+    ncol=256 ;
+    
+    % hot cold colormap
+    cmap=hsv2rgb([linspace(2/3, 0, ncol)' 0.9*ones(ncol,1) ones(ncol,1)]) ;
+    cmap(1,:)=[0 0 0] ;
+    colormap(cmap) ;
+    
+    corr = correlation(P);
+    imagesc(abs(corr), [0 1]) ;  % no sign
+    
+    axis image;
+    colorbar ;
 
 end
 
@@ -369,8 +410,8 @@ end
 %-------------------------------------------------------------------------
 
 function Corr=correlation(Cov)
-
-sigmas = sqrt(diag(Cov));
-Corr = diag(1./sigmas)*Cov*diag(1./sigmas);
+    
+    sigmas = sqrt(diag(Cov));
+    Corr = diag(1./sigmas)*Cov*diag(1./sigmas);
 
 end
