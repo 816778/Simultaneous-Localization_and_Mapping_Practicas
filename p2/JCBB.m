4bb32d4a62b1f734cc38bde58b88195d75f7978f
816778 <816778@unizar.es>
2025-02-21 10:12:34 +0100

    jcbb

--- p2/JCBB.m
+++ p2/JCBB.m
@@ -25,6 +25,16 @@ if i > observations.m % leaf node?
     end
 else
     % complete JCBB here
+    for j = 1:prediction.n % for j in {1...n}
+        % if individual_compatibility(i,j) and then joint_compatibility(H, i, j)
+        if compatibility.ic(i, j) && jointly_compatible(prediction, observations, [H j])
+            JCBB_R (prediction, observations, compatibility, [H j], i+1); % pairing (E_i, F_j) accepted
+        end
+    end
+    % if pairing(H) + m - i > pairing(Best)
+    if pairings(H) + observations.m - i > pairings(Best.H) % can do better?
+        JCBB_R (prediction, observations, compatibility, [H 0] , i+1);  % start node, E_i nor paired
+    end
 end
 
 %-------------------------------------------------------
